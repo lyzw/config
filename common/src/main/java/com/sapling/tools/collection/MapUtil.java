d60c986b77d153ea52b4fc865291c0e47fa0e410
zhouwei <wei.zhou@eoitek.com>
2018-07-02 10:33:16 +0800

    初始化

---
+++ common/src/main/java/com/sapling/tools/collection/MapUtil.java
@@ -0,0 +1,76 @@
+package com.sapling.tools.collection;
+
+import com.sapling.tools.common.ReflectUtil;
+
+import java.util.Map;
+
+/**
+ * @author zhouwei
+ * @version v1.0
+ * @date 2018/6/7
+ * @since v1.0
+ */
+public class MapUtil {
+
+    /**
+     * 将对象的属性与值转换为map
+     *
+     * @param object 对象数据
+     * @return 对象数据的map
+     */
+    public static Map objectToMap(Object object) {
+        return ReflectUtil.getFieldValueMap(object);
+    }
+
+    /**
+     * 将map按照k-v，实例化一个实例对象
+     *
+     * @param clazz 对象类型
+     * @param map   名称-值map对象
+     * @return 对象实例
+     * @throws InstantiationException 实例化异常
+     * @throws IllegalAccessException 访问异常
+     */
+    public static Object mapToObject(Class clazz, Map<String, Object> map)
+            throws InstantiationException, IllegalAccessException {
+        return ReflectUtil.getInstance(clazz, map);
+    }
+
+
+    /**
+     * 比较两个map的数据是否相同
+     *
+     * @param one
+     * @param two
+     * @return
+     */
+    public static boolean compare(Map<Object, Object> one, Map<Object, Object> two) {
+        if (one == null && two == null) {
+            return true;
+        } else if (one == null || two == null) {
+            return false;
+        }
+        if (one.size() != two.size()) {
+            return false;
+        }
+        for (Map.Entry<Object, Object> entry : one.entrySet()) {
+            Object key = entry.getKey();
+            if (!two.containsKey(key)) {
+                return false;
+            }
+            Object value = two.get(key);
+            Class clazz = value.getClass();
+            if (clazz.isInstance(entry.getValue())) {
+                //能否所有的都用equals？
+                if (value.equals(entry.getValue())) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ common/src/main/java/com/sapling/tools/common/StringUtil.java
@@ -0,0 +1,16 @@
+package com.sapling.tools.common;
+
+/**
+ * @author zhouwei
+ * @version v1.0
+ * @date 2018/6/6
+ * @since v1.0
+ */
+public class StringUtil {
+
+    public static boolean isEmpty(String value) {
+        return !(value != null && !"".equals(value));
+    }
+
+
+}
